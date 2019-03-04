80daee5be33e8be108d97b11be4b98a37f8c273a
MaksimCha <LuckyRogerStar@yandex.ru>
2019-03-05 00:20:05 +0300

    upload file function

--- src/main/java/common/FileProvider.java
+++ src/main/java/common/FileProvider.java
@@ -1,4 +1,30 @@
 package common;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class FileProvider {
+
+    public static Stream<File> uploadFile(int fileNameLong, int fileLong) {
+        byte[] array = new byte[fileNameLong]; // length is bounded by 7
+        new Random().nextBytes(array);
+        byte[] fileByteArray = new byte[fileLong]; // length is bounded by 7
+        new Random().nextBytes(fileByteArray);
+        String generatedName = new String(array, Charset.forName("UTF-8"));
+        File file = new File("*/src/main/java/resources/files/" + generatedName + ".txt");
+        try (FileOutputStream fos = new FileOutputStream(file)){
+            fos.write(fileByteArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Stream.of(file);
+    }
+
+    public static Stream<File> uploadFile(String pathName){
+        return Stream.of(new File(pathName));
+    }
 }
