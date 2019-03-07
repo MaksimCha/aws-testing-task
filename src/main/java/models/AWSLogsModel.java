71129003d41977eb392ebf9f563a2a97c66d2909
Maksim_Cha <Maksim_Cha@epam.com>
2019-03-07 19:16:01 +0300

    Add waiter of result for correct working

--- src/main/java/models/AWSLogsModel.java
+++ src/main/java/models/AWSLogsModel.java
@@ -7,19 +7,32 @@ import com.amazonaws.services.logs.model.FilterLogEventsResult;
 
 import java.io.File;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AWSLogsModel {
 
     private String clientRegion = System.getProperty("client.region");
     private AWSLogs awsLogsClient;
+    FilterLogEventsRequest request;
+    FilterLogEventsResult result;
 
     public void checkLambdaLog(File file) {
         awsLogsClient = AWSLogsClientBuilder.standard().withRegion(clientRegion).build();
-
-        FilterLogEventsRequest request = new FilterLogEventsRequest().withInterleaved(true)
-                .withLogGroupName("/aws/lambda/FunctionHandler").withFilterPattern("LAMBDA FileName");
-        FilterLogEventsResult result = awsLogsClient.filterLogEvents(request);
+        request = new FilterLogEventsRequest().withLogGroupName("/aws/lambda/FunctionHandler").withFilterPattern("LAMBDA " + file.getName());
+        long start = System.currentTimeMillis();
+        while (!checkArray(request)) {
+            long finish = System.currentTimeMillis();
+            long timeConsumedMillis = finish - start;
+            if(timeConsumedMillis > 30000) {
+                break;
+            }
+        }
         result.getEvents().forEach(System.out::println);
-        System.out.println(result.getEvents().get(result.getEvents().size() - 1).getMessage());
-        //assertTrue(result.getEvents().get(result.getEvents().size()-1).getMessage().contains("LAMBDA FileName:" + file.getName()));
+        assertTrue(result.getEvents().get(0).getMessage().contains("LAMBDA FileName:" + file.getName()));
+    }
+
+    public boolean checkArray(FilterLogEventsRequest request) {
+        result = awsLogsClient.filterLogEvents(request);
+        return result.getEvents().size() >= 1;
     }
 }
